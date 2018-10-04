78807f03ef4396f90395960184da79a3dd62bc1c
Larry Hu <larry@shyft.network>
2018-10-04 15:54:15 +0000

    Update init and start scripts

--- shyft-cli/initShyftGeth.sh
+++ shyft-cli/initShyftGeth.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-if [[ -z "${DBENV}" ]]; then
-  ./build/bin/geth --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ShyftNetwork.json
-else
   /bin/geth --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ShyftNetwork.json
-fi
+
