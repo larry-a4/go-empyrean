27082790e93a75a9474399c30d45ec07b4538001
Tim Williams <tim@chainsafe.io>
2018-12-16 17:19:29 -0500

    change directory structure and docker images

--- shyft-files/shyft-cli/initShyftGeth.sh
+++ shyft-files/shyft-cli/initShyftGeth.sh
@@ -2,11 +2,11 @@
 if [ -z "${DBENV}" ]; then
   ./build/bin/geth --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ./ShyftNetwork.json
 else
-  if [ -d /go/src/ShyftNetwork/go-empyrean/shyftData/geth/chaindata ]; then
+  if [ -d /go-empyrean/shyftData/geth/chaindata ]; then
     echo "Skipping Genesis Initialization as already completed"
     :
   else
     echo "Initializing Custom Genesis Block"
-    /bin/geth --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ShyftNetwork.json
+    /bin/geth --identity "ShyftTestnetNode" --datadir "./shyftData" init ./shyft-files/ShyftNetwork.json
   fi
 fi
