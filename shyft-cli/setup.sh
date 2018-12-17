7d46c7b0d17f5c016c9f27cd8e1d55fe5cc39016
Tim Williams <tim@chainsafe.io>
2018-12-17 16:40:47 -0500

    refactor start scripts and docker

--- shyft-cli/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set +e
-dropdb 'shyftdb'
-sh ./shyft-cli/resetShyftGeth.sh &&                     # Reset geth data - Remove pg and chain data
-sh ./shyft-cli/initShyftGeth.sh                         # Init Shyft Geth
